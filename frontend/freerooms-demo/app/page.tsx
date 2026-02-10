9ac127bca988c6f940863d90b33367420b7f5f96
Bassem Kidwai <z5479692@au.unsw.edu.au>
2026-02-10 22:20:52 +1100

    Added buttons to the nav bar (yet to be properly styled)

--- frontend/freerooms-demo/app/page.tsx
+++ frontend/freerooms-demo/app/page.tsx
@@ -1,4 +1,10 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  Grid2X2,
+  Map,
+  Search,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,7 +12,22 @@ export default function Home() {
     <>
       <div>
         {/* navbar */}
-        <ModeToggle />
+        <div>
+          {/* logo component */}
+        </div>
+        <div className="flex flex-row gap-x-1">
+          {/* navbar buttons */}
+          <Button variant="outline" size="icon">
+            <Search />
+          </Button>
+          <Button variant="outline" size="icon">
+            <Grid2X2 />
+          </Button>
+           <Button variant="outline" size="icon">
+            <Map />
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
       <div>
         {/* filters/search/sort */}
