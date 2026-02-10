3d2ead63bebafba2796121a5a750a2c9f0aeba70
Bassem Kidwai <z5479692@au.unsw.edu.au>
2026-02-10 22:08:36 +1100

    Added a basic skeleton structure to the home page, and installed shadcn

--- frontend/freerooms-demo/app/page.tsx
+++ frontend/freerooms-demo/app/page.tsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <div>
+        {/* navbar */}
+      </div>
+      <div>
+        {/* filters/search/sort */}
+      </div>
+      <div>
+        {/* grid */}
+      </div>
     </>
   );
 }
