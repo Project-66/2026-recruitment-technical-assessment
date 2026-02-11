0f5500a8352d0177274d2dae4c11ab758a0dc250
Bassem Kidwai <z5479692@au.unsw.edu.au>
2026-02-11 15:41:02 +1100

    Fixed shadcn colour scheme, and completed the Navbar (including Stretch Task 1). Haven't made it responsive to smaller screen sizes yet.

--- frontend/freerooms-demo/app/page.tsx
+++ frontend/freerooms-demo/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import {
@@ -6,25 +8,40 @@ import {
   Search,
 } from "lucide-react";
 import Image from "next/image";
+import FreeroomLogoOpen from "@/assets/freeRoomsLogo.png";
+import FreeroomLogoClosed from "@/assets/freeroomsDoorClosed.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [ doorOpen, setDoorOpen ] = useState(true);
+
   return (
     <>
-      <div>
+      <div className="p-3 pb-2 w-screen flex flex-row items-center border-b">
         {/* navbar */}
-        <div>
-          {/* logo component */}
+        <div className="flex flex-row h-max items-center">
+          <Image
+            src={doorOpen ? FreeroomLogoOpen : FreeroomLogoClosed}
+            alt="freerooms-logo"
+            width={40}
+            onClick={() => setDoorOpen(!doorOpen)}
+          />
+          <span className="text-[#ef7020] text-3xl font-bold">Freerooms</span>
         </div>
-        <div className="flex flex-row gap-x-1">
+        <div className="flex-1">
+
+        </div>
+        <div className="flex flex-row gap-x-2">
           {/* navbar buttons */}
+          {/* TODO: switch icons to the ones provided in README (google fonts ones) */}
           <Button variant="outline" size="icon">
-            <Search />
+            <Search color="#ef7020"/>
           </Button>
-          <Button variant="outline" size="icon">
-            <Grid2X2 />
+          <Button variant="default" size="icon" className="bg-[#ef7020]">
+            <Grid2X2 color="white"/>
           </Button>
            <Button variant="outline" size="icon">
-            <Map />
+            <Map color="#ef7020"/>
           </Button>
           <ModeToggle />
         </div>
