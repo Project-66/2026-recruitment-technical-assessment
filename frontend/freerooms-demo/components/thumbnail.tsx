9604d88c5ea797a96b1af6782a8a003f8be252d5
Bassem Kidwai <z5479692@au.unsw.edu.au>
2026-02-25 14:08:53 +1100

    Created a building interface and a stub file for the thumbnails

---
+++ frontend/freerooms-demo/components/thumbnail.tsx
@@ -0,0 +1,12 @@
+import Building from "@/interfaces/building";
+
+export default function Thumbnail({
+  buildingInfo
+} : {
+  buildingInfo: Building
+}) {
+  return (
+    <div>
+    </div>
+  );
+}
---
+++ frontend/freerooms-demo/interfaces/building.ts
@@ -0,0 +1,5 @@
+export default interface Building {
+  name: string;
+  rooms_available: number;
+  building_picture: string;
+}
